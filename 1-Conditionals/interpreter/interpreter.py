9db6124fbe5637241de4f21c222f5717b5099a84
Braj S <159206352+BrajSapkota@users.noreply.github.com>
2026-02-17 02:42:00 +0000

    Upload completed interpreter.py

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,12 @@
+x = float(input("input x: "))
+y = input("input *, /, +, - : ")
+z = float(input("input z: "))
+
+if y == "+":
+    print(x + z)
+elif y == "-":
+    print(x - z)
+elif y == "*":
+    print(x * z)
+elif y == "/":
+    print(x / z)
