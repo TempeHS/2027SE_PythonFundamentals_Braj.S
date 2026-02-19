5a9169e1d3ae3cdbecc7a7c8511fdd2b0299ed25
Braj S <159206352+BrajSapkota@users.noreply.github.com>
2026-02-19 02:33:55 +0000

    Upload completed meal.py

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -3,19 +3,18 @@ def main():
 
     if convert(time) <= 8:
         print("Breakfast time")
-
-
-#    elif 12 <= ConvertedTime <= 13:
-#        print("Lunch Time")
-#    elif 18 <= ConvertedTime <= 19:
-#        print("Dinner Time")
-#    else:
-#        print("Nothing")
+    elif 12 <= convert(time) <= 13:
+        print("Lunch Time")
+    elif 18 <= convert(time) <= 19:
+        print("Dinner Time")
+    else:
+        print("Nothing")
 
 
 def convert(t):
-    hours, minutes = time.split(":")
+    hours, minutes = t.split(":")
     t = int(hours) + (int(minutes) / 60)
+    return t
 
 
 main()
