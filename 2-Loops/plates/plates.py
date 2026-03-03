d6dd01dd7bff1b87c328d63283a0f43732b410dc
Braj S <159206352+BrajSapkota@users.noreply.github.com>
2026-03-03 02:36:24 +0000

    Upload completed plates.py

--- 2-Loops/plates/plates.py
+++ 2-Loops/plates/plates.py
@@ -7,7 +7,12 @@ def main():
 
 
 def is_valid(s):
-    if characters(s) == True and length(s) == True:
+    if (
+        first_characters(s) == True
+        and length(s) == True
+        and number_order(s) == True
+        and numbers_at_end(s) == True
+    ):
         return True
 
 
@@ -16,16 +21,31 @@ def length(c):
     for letters in c:
         if letters.isalnum():
             letter_count += 1
-    return letter_count <= 6
+    return letter_count <= 6 and c.isalnum()
 
 
-def characters(c):
-    letter_count = 0
-    for letters in c:
-        if letters.isalpha():
-            print(letters)
-            letter_count += 1
-    return letter_count >= 2
+def first_characters(c):
+    if len(c) >= 2 and c[:2].isalpha():
+        return True
+
+
+def number_order(c):
+    for ch in c:
+        if ch.isdigit():
+            return ch != "0"
+    return True
+
+
+def numbers_at_end(c):
+    seen_digit = False
+
+    for ch in c:
+        if ch.isdigit():
+            seen_digit = True
+        elif seen_digit and ch.isalpha():
+            return False
+
+    return True
 
 
 main()
