915957b04b84f7d7a8f36ba6838a74d39d1643d3
Braj S <159206352+BrajSapkota@users.noreply.github.com>
2026-03-21 03:39:52 +0000

    Upload completed taqueria.py

--- 3-Exceptions/fuel/fuel.py
+++ 3-Exceptions/fuel/fuel.py
@@ -3,25 +3,21 @@ def main():
         numerator, denominator = input("What's the fraction? ").split("/")
         numerator = int(numerator)
         denominator = int(denominator)
-    except ValueError:
+    except (ValueError, ZeropercentageError):
         main()
 
     else:
-        if denominator == 0:
+        percentage = (numerator / denominator) * 100
+        if percentage < 0 or percentage > 100:
             main()
+        elif percentage == 0:
+            print("E")
+        elif percentage == 100:
+            print("F")
+        elif percentage % 1 == 0:
+            print(f"{percentage:g}%")
         else:
-            division = numerator / denominator
-            percentage = (division) * 100
-            if division < 0 or division > 1:
-                main()
-            elif division == 0:
-                print("E")
-            elif division == 1:
-                print("F")
-            elif percentage % 1 == 0:
-                print(f"{percentage:g}%")
-            else:
-                print(f"{round(percentage)}%")
+            print(f"{round(percentage)}%")
 
 
 main()
